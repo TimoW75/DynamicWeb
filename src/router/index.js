6d86ff468f21013f91b8a25118e5f94ffe12d460
Timo Werkhoven <timo2003werkhoven@gmail.com>
2024-05-31 11:57:20 +0200

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('DynamicWeb'),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
